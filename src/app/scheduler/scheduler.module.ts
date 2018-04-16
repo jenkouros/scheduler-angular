bd4741595ed8f57a5a268da59e0cd236ba6277d9
Aleš Gaber <ales.gaber@gmail.com>
2018-04-16 12:26:49 +0200

    popravek

--- src/app/scheduler/scheduler.module.ts
+++ src/app/scheduler/scheduler.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from "../shared/shared.module";
-import { StoreModule } from "@ngrx/store";
-import * as fromStore from "./store";
+import { SharedModule } from '../shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromStore from './store';
 import * as fromComponents from './components';
-import { SchedulerRouterModule } from "./scheduler-router.module";
-import * as fromServices from "./services";
-import { HttpClientModule } from "@angular/common/http";
-import { PlanitemListComponent } from "./components";
-import { EffectsModule } from "@ngrx/effects";
+import { SchedulerRouterModule } from './scheduler-router.module';
+import * as fromServices from './services';
+import { HttpClientModule } from '@angular/common/http';
+import { PlanitemListComponent } from './components';
+import { EffectsModule } from '@ngrx/effects';
 
 // Dx Component
 import { DxPopupModule,
@@ -27,7 +27,7 @@ import { DxPopupModule,
         HttpClientModule,
         StoreModule.forFeature('scheduler', fromStore.reducers),
         EffectsModule.forFeature(fromStore.effects),
-        DxSchedulerModule, DxButtonModule, DxTemplateModule, DxLinearGaugeModule
+        DxSchedulerModule, DxButtonModule, DxTemplateModule, DxLinearGaugeModule, DxPopupModule, DxDataGridModule
     ],
     declarations: [...fromComponents.components],
     providers: [...fromServices.services]
