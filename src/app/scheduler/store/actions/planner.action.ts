a6617f497f953b18899403727694898725a1ba38
jenko.uros@gmail.com <uje@UJEWIN10>
2018-04-16 10:04:16 +0200

    Add planner actions

---
+++ src/app/scheduler/store/actions/planner.action.ts
@@ -0,0 +1,14 @@
+export const LOAD_CONTAINERS = "[Planner] Load containers";
+export const LOAD_CONTAINERS_FAIL = "[Planner] Load containers Fail";
+export const LOAD_CONTAINERS_SUCCESS = "[Planner] Load containers Success";
+export const SELECT_CONTAINERS = "[Planner] Select containers";
+export const DESELECT_CONTAINERS = "[Planner] Deselect containers";
+
+export const LOAD_PLANNINGITEMS = "[Planner] Load planning items";
+export const LOAD_PLANNINGITEMS_FAIL = "[Planner] Load planning items fail";
+export const LOAD_PLANNINGITEMS_SUCCESS = "[Planner] Load planning items success";
+export const ADD_PLANNINGITEMS = "[Planner] Add planning items";
+export const REMOVE_PLANNINGITEM = "[Planner] Remove planning item";
+export const PLAN_PLANNINGITEM = "[Planner] Plan planning item";
+
+export const LOAD_PLANNEDITEMS = "[Planner] Load planned items";
