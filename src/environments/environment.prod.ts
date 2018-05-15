435a4a44be4f99364eec5af7a4183d582fce8bd1
Uros Jenko <uje@sisteh.local>
2018-05-15 09:40:46 +0200

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiUrl: 'http://10.99.1.54:21000/api'
-=======
-  apiUrl: 'http://10.10.20.11:21000/api'
->>>>>>> d9324a51c4fc2f6b9673b8b7da93e10fe0e38844
 };
