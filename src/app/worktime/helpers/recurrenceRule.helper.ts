6e4b982db746830753829545e3303f65f183a5ba
Uros Jenko <uje@sisteh.local>
2019-03-26 14:04:30 +0100

    IE support

--- src/app/worktime/helpers/recurrenceRule.helper.ts
+++ src/app/worktime/helpers/recurrenceRule.helper.ts
@@ -1,5 +1,5 @@
 import { RRule, Weekday, Frequency, Options } from 'rrule';
-import { Language } from '../../../../node_modules/rrule/dist/esm/nlp/i18n';
+// import { Language } from '../../../../node_modules/rrule/dist/esm/nlp/i18n';
 
 export class RecurrenceRule {
   static Parse(rfcString): any {
@@ -90,7 +90,7 @@ export function getTextById(id: string) {
   return slovenianStrings[id] || id;
 }
 
-export const slovenian: Language = {
+export const slovenian /*: Language*/ = {
   dayNames: [
     'nedeljo',
     'ponedeljek',
